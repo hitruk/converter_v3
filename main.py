cd97ce4adf505fbcfec539b5aff328f24ad9af2e
Andrey Hitruk <abakhtiev@mail.ru>
2024-04-23 19:20:29 +0300

    Moded file: main.py .gitignore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.constants import *
+import re
 
 class Model:
     def __init__(self, value):
@@ -10,7 +11,8 @@ class Model:
             # значение(value) должно быть целым числом или числом с плавающей запятой.
             raise TypeError(f'In class Model, method __init__: value must be an integer or float')
         if value < 0:
-            # значение(value) не должно быть отрицательным
+            # значение(value) не должно быть отрицательным, 
+            # но только как прараметр класса, при создании экземпляра класса. 
             raise ValueError(f'value must not be negative')
 
         self._value = value
@@ -54,24 +56,28 @@ class View(ttk.Frame):
         self.label_right.grid(column=2, row=1, **options)
 
         # ENTRY LEFT
+        vcmd = (self.register(self.validate_left), '%P')
+        #ivcmd = (self.register(self.on_invalid),)
         self.entry_left_type = tk.StringVar(value=1)
         self.entry_left = ttk.Entry(self, textvariable=self.entry_left_type)
         self.entry_left.bind('<KeyRelease>', self.change_entry_left)
+        self.entry_left.config(validate='key', validatecommand=vcmd)#, invalidcommand=ivcmd)
         self.entry_left.grid(column=0, row=2, **options)
 
-
-        # Label_swap
-        self.label_swap_type = tk.BooleanVar() # defoult False
-        self.label_swap = ttk.Label(self,  text='<==>') #, textvariable=self.label_swap_type)
-        self.label_swap.grid(column=1, row=2)#, **options)
-        # разобраться с bind !!!
-        self.label_swap.bind('<Button-1>', self.label_swap_cliked)
-
         # ENTRY RIGHT
+        vcmd_r = (self.register(self.validate_right), '%P')
         self.entry_right_type = tk.StringVar(value=1.609344)
         self.entry_right = ttk.Entry(self, textvariable=self.entry_right_type)
         self.entry_right.bind('<KeyRelease>', self.change_entry_right)
+        self.entry_right.config(validate='key', validatecommand=vcmd_r)
         self.entry_right.grid(column=2, row=2, **options)
+
+        # Label_swap
+        self.label_swap_type = tk.BooleanVar() # defoult False
+        self.label_swap = ttk.Label(self,  text='<==>')
+        self.label_swap.grid(column=1, row=2)
+        self.label_swap.bind('<Button-1>', self.label_swap_cliked)
+
 
         # set controller
         self.controller = None
@@ -79,14 +85,16 @@ class View(ttk.Frame):
         # swith converter необходим для переключения конвертера mi_to_km/km_to_mi
         self.switch = False
 
+        print(f'Entry left value defoult  {self.entry_left_type.get()}')
+
     def set_controller(self, controller):
         self.controller = controller
 
     def change_entry_left(self, event):
-        print('entry left')
+        print(f'entry_left_get {self.entry_left.get()}')
         if self.controller:
             try:
-                if self.entry_left_type.get() == '':
+                if self.entry_left_type.get() == '' or self.entry_left_type.get() == '.':
                     self.entry_right_type.set('?')
                     return
                 entry_left_value = float(self.entry_left.get())
@@ -98,14 +106,13 @@ class View(ttk.Frame):
         print('entry right')
         if self.controller:
             try:
-                if self.entry_right_type.get() == '':
+                if self.entry_right_type.get() == '' or self.entry_right_type.get() == '.':
                     self.entry_left_type.set('?')
                     return
                 entry_right_value = float(self.entry_right.get())
                 self.controller.convert_right(entry_right_value)
             except ValueError as error: # TypeError as error:
                 print(f'In class View, method name: change_entry_right {error}')
-    
 
     def label_swap_cliked(self, event):
         '''switch swap'''
@@ -116,7 +123,7 @@ class View(ttk.Frame):
                     self.entry_right_type.set(1.609344)
                     self.label_left['text'] = 'miles (mi)'
                     self.label_right['text'] = 'kilometers (km)'
-                    self.label_title['text'] = 'Convert Miles to Km' 
+                    self.label_title['text'] = 'Convert Miles to Km'
                     self.switch = False
                 else:
                     self.entry_right_type.set(0.62137119)
@@ -127,12 +134,42 @@ class View(ttk.Frame):
             except ValueError as error: # TypeError as error:
                 print(f'In class View, method name: label_swap_cliked {error}')
 
-
     def entry_right_value(self, value):
         self.entry_right_type.set(value) 
 
     def entry_left_value(self, value):
         self.entry_left_type.set(value)
+    
+    #  /* VALIDATE ENTRY
+    def validate_left(self, value):
+        ''' Validate entry left '''
+        if re.search(r'[^\d \.]', value):
+            return False
+
+        if re.search(r' ', value):
+            return False
+
+        res = re.findall(r'\.', value)
+        if len(res) > 1:
+            self.entry_right_type.set('?')
+            return False
+        return True
+
+
+    def validate_right(self, value):
+        ''' Validate entry right '''
+        if re.search(r'[^\d \.]', value):
+            return False
+
+        if re.search(r' ', value):
+            return False
+
+        res = re.findall(r'\.', value)
+        if len(res) > 1:
+            self.entry_left_type.set('?')
+            return False
+        return True
+    # */ END Validate
 
 
 class Controller:
@@ -174,7 +211,7 @@ class Controller:
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title('Hello world!')
+        self.title('Hello converter!')
         self.geometry('400x400')
         
         model = Model(1)
@@ -188,4 +225,3 @@ class App(tk.Tk):
 if __name__ == '__main__':
     app = App()
     app.mainloop()
-
